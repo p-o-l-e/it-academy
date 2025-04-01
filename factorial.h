4a8e318cc7cf3d3f6ddf1b428355cecc1df813ea
p-o-l-e <ant.float.farm@gmail.com>
2025-04-01 18:03:22 +0300

    Update factorial.h

--- factorial.h
+++ factorial.h
@@ -1,20 +1,27 @@
 #pragma once
 #include <iostream>
+#include <chrono>
+#include <cmath>
+#include <climits>
 
+#define F_BIG_INTEGER
 #ifdef F_BIG_INTEGER
 
-#include <string>
-const std::string string_multiply(const std::string& a, const std::string& b)
-const std::string factorial(const int& n);
+    #define F_INIT 0 
+    #include <vector>
+    #include <string>
+    #include <algorithm>
+    const std::string string_multiply(const std::string& a, const std::string& b);
+    const std::string factorial(const int& n);
 
 #elif defined F_FLOAT
+    #define F_INIT 0.0f
 
-#include <cmath>
 const double factorial(const double& n);
 
 #else
-const long factorial(const int& n);
+    #define F_INIT 0
+    const long factorial(const int& n);
 
 #endif
-
 
